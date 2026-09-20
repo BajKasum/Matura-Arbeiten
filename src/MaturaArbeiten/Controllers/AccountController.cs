b13367bacc727d77687e7af6c43d8d4012cea350
BajKasum <kasumbajrami7@gmail.com>
2026-09-20 21:42:33 +0200

    Anmeldeseite nach UI Mockup 1 erstellen

---
+++ src/MaturaArbeiten/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MaturaArbeiten.Controllers;
+
+public class AccountController : Controller
+{
+    public IActionResult Anmelden()
+    {
+        return View();
+    }
+}
