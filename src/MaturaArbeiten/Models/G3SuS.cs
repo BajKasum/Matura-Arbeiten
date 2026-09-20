5e1cfb40d486b4137a3f4a559c2de1c3aa936dc3
BajKasum <kasumbajrami7@gmail.com>
2026-09-20 09:18:47 +0200

    Model G3SuS erstellen

---
+++ src/MaturaArbeiten/Models/G3SuS.cs
@@ -0,0 +1,13 @@
+﻿namespace MaturaArbeiten.Models;
+
+public class G3SuS
+{
+    public int Id { get; set; }
+    public string Vorname { get; set; } = string.Empty;
+    public string Nachname { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string? PasswortHash { get; set; }
+    public bool PasswortGeaendert { get; set; }
+    public int Fehlversuche { get; set; }
+    public DateTime? GesperrtBis { get; set; }
+}
