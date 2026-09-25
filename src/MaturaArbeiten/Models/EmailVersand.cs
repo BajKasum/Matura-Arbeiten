d2a2ac0cb09e1a854e5afef7a499440eb8102e4e
BajKasum <kasumbajrami7@gmail.com>
2026-09-25 11:30:41 +0200

    Model EmailVersand erstellen

---
+++ src/MaturaArbeiten/Models/EmailVersand.cs
@@ -0,0 +1,10 @@
+﻿namespace MaturaArbeiten.Models;
+    public class EmailVersand
+    {
+    public int Id { get; set; }
+    public int g3susId { get; set; }
+    public string Typ { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+    public DateTime GesendetAm { get; set; }
+    }
+
