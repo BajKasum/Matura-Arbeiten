096771d3bb756406dcefa79d8e10432594fb127b
BajKasum <kasumbajrami7@gmail.com>
2026-09-25 11:30:15 +0200

    Model Praesentation erstellen

---
+++ src/MaturaArbeiten/Models/Praesentation.cs
@@ -0,0 +1,11 @@
+﻿namespace MaturaArbeiten.Models;
+
+public class Praesentation
+{
+    public int Id { get; set; }
+    public string Thema { get; set; } = string.Empty;
+    public DateOnly Datum { get; set; }
+    public TimeOnly Uhrzeit { get; set; }
+    public string Raum { get; set; } = string.Empty;
+    public List<Eintragung> Eintragungen { get; set; } = [];
+}
