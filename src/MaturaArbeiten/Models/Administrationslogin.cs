225b469f78cbd27dbb1ea7486034a6350effd676
BajKasum <kasumbajrami7@gmail.com>
2026-09-25 11:43:56 +0200

    Model Administrationslogin erstellen

---
+++ src/MaturaArbeiten/Models/Administrationslogin.cs
@@ -0,0 +1,12 @@
+﻿namespace MaturaArbeiten.Models;
+    public class Administrationslogin
+    {
+    public Id { get; set; }
+    public string Vorname { get; set; } = string.Empty;
+    public string Nachname { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty; //wert darf nicht null sein .Empty
+    public string? PasswortHash { get; set; }
+    public int Fehlversuche { get; set; }
+    public DateTime? GesperrtBis { get; set; } //wert darf null sein ?
+}
+
