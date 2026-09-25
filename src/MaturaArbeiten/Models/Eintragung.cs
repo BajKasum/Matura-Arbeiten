d1542a8d27583664d9d37ac51e81de87f6e7cc24
BajKasum <kasumbajrami7@gmail.com>
2026-09-25 11:29:56 +0200

    Model Eintragung erstellen

---
+++ src/MaturaArbeiten/Models/Eintragung.cs
@@ -0,0 +1,9 @@
+﻿namespace MaturaArbeiten.Models;
+
+public class Eintragung
+{
+    public int Id { get; set; }
+    public int G3susId { get; set; }
+    public int PraesentationId { get; set; }
+    public DateTime ErstelltAm { get; set; }
+}
